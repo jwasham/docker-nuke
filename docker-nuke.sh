2b001580649b70c7d0bd4f587420e0fd56f0ed46
John Ingram <john.ingram@adtran.com>
2020-02-11 13:47:08 -0600

    Forced removal of running containers/images. Added removal of networks

--- docker-nuke.sh
+++ docker-nuke.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-docker rm -v $(docker ps -aq)
+docker rm -f -v $(docker ps -aq)
 
-docker rmi $(docker images -q)
+docker rmi -f  $(docker images -q)
 
 docker volume rm $(docker volume ls -q)
+
+docker network rm $(docker network ls -q)
